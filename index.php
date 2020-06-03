4b11bbc4c6b26d79b42136be54e198e002a46a97
santiago alejandro <santi20alejo@hotmail.com>
2020-06-03 02:34:08 -0500

    kill me

--- index.php
+++ index.php
@@ -4,39 +4,17 @@
     $user ="root";
     $pw = "matias2014se98";
     $db ="movil";
-    function Conectarse($host,$puerto,$user,$pw,$db) {
-    if (!($link = mysqli_connect($host.":".$puerto, $user, $pw))){ 
-            echo "Error conectando a la base de datos.<br>"; 
-           exit(); 
-         }else{
-          echo " ";
-          }
-          if (!mysqli_select_db($link, $db)) 
-        { 
-            echo "Error seleccionando la base de datos.<br>"; 
-            exit(); 
-        }else{
-           echo " "; 
-         }
-       return $link; 
+   
+$connect = new mysqli("23.236.50.121","root","matias2014se98","movil");
+
+if($connect){
+   
+}else{
+  echo "Fallo, revise ip o firewall";
+  exit();
 }
-//conectarse a la bd
-$link = Conectarse($host,$puerto,$user,$pw,$db);
-//metodo para ver los datos
-    
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $correo=$_POST['correo'];
     $password=$_POST['password'];
-    $query='SELECT * FROM usuarios WHERE correo="'.$correo.'" && pass="'.$password.'"';
-    $result = mysqli_query($link,$query);
-    $resultado=array();
-    while($extraerDatos=$result->fetch_assoc()){
-        $resultado[]=$extraerDatos;
-        $json = json_encode($resultado);
-        echo $json;
-    }
-        
-    
-}
+    $connect->query('SELECT * FROM usuarios WHERE correo="'.$correo.'" && pass="'.$password.'"');
 
     ?>
