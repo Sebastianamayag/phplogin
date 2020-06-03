f9a50342dfcc5ae328cc7b5b229f44c7f8e118c9
santiago alejandro <santi20alejo@hotmail.com>
2020-06-03 01:01:16 -0500

    cambios

--- login.php
+++ login.php
@@ -1,28 +1,37 @@
 <?php
-
-    //include 'conexion.php';
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $connect = new mysqli("23.236.50.121","root","matias2014se98","movil");
-
-    if($connect){
-    }else{
-    echo "Fallo, revise ip o firewall";
-    exit();
-    }
-    $correo = $_POST['correo'];
-    $password = $_POST['pass'];
+    $host ="23.236.50.121";
+    $puerto = "3306";
+    $user ="root";
+    $pw = "matias2014se98";
+    $db ="movil";
+    function Conectarse($host,$puerto,$user,$pw,$db) {
+    if (!($link = mysqli_connect($host.":".$puerto, $user, $pw))){ 
+            echo "Error conectando a la base de datos.<br>"; 
+           exit(); 
+         }else{
+          echo " ";
+          }
+          if (!mysqli_select_db($link, $db)) 
+        { 
+            echo "Error seleccionando la base de datos.<br>"; 
+            exit(); 
+        }else{
+           echo " "; 
+         }
+       return $link; 
+}
+//conectarse a la bd
+    $link = Conectarse($host,$puerto,$user,$pw,$db);
+//metodo para ver los datos
     
-
-    $consultar=$connect->query('SELECT * FROM usuarios WHERE correo="'.$correo.'" && pass="'.$password.'"');
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $query="SELECT * FROM productos WHERE catergoriaProductos=$categoria";
+    $result = mysqli_query($link,$query);
     $resultado=array();
-
-    while($extraerDatos=$consultar->fetch_assoc()){
+    while($extraerDatos=$result->fetch_assoc()){
         $resultado[]=$extraerDatos;
     }
-
     echo json_encode($resultado);
-
     }
 
     ?>
